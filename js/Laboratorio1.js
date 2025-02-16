8e9b98066db26af5a2434a2ba77644697e544014
Marel52 <151053738+Marel52@users.noreply.github.com>
2025-02-15 21:20:38 -0600

    nuevo archivo Lab1.css

---
+++ js/Laboratorio1.js
@@ -0,0 +1,25 @@
+// console (log, info, warn, error, assert)
+console.log("Aquí se habla de mí");
+console.log("Yo sirvo para muchas cosas");
+console.warn("Cuidado con las plantas venenosas");
+console.error("No las vayas a ahogar");
+console.assert(1 == 1); 
+console.assert(1 === 1);
+console.assert(1 == "1");
+
+//Variables, constantes 
+const precio = 99.9 // Las constantes no pueden ser modificadas
+
+//Alcance de las variables 
+for (let i=0; i<10; i++){
+    console.log(i);             //CONTADOR DEL 1-9 
+} 
+
+
+//alert, prompt, confirm
+alert("hola"); //Mensaje antes de cargar
+
+
+
+
+
