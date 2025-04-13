f928e627dc149b4fe94feacef6f74a6574b8e3d1
Marel52 <151053738+Marel52@users.noreply.github.com>
2025-04-13 09:06:06 -0600

    Verificar existencia del usuario e implementación del catch por si hay un error al registrar

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -9,3 +9,19 @@ exports.getRegister = (request, response, next) => {
     request.session.error = null;
 };
 
+exports.postRegister = async (request, response, next) => {
+    try {
+        const name = request.body.nombre;
+        const password = request.body.password;
+
+        const [existingUsers] = await User.findByName(name);
+        
+        if (existingUsers.length > 0) {
+            request.session.error = 'El nombre de usuario ya está en uso.';
+            return response.redirect('/user/register');
+        }
+
+    }catch (error) {
+        console.error('Error en el registro', error);
+    }
+};
